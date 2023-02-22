cd8a5ecfe1f72bbfabfba71b863c7092be0b727e
nicmach <morilanger2005@gmail.com>
2023-02-22 20:13:42 +0100

    Adding a debug menu for three.js objects

--- Website/Base/src/script.js
+++ Website/Base/src/script.js
@@ -2,8 +2,25 @@ import * as THREE from 'three'
 import { WebGLMultisampleRenderTarget } from 'three'
 import { randFloat, randInt } from 'three/src/math/MathUtils'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import GUI from 'lil-gui'
 
-// Cursor
+/*
+Debug GUI
+*/
+
+const gui = new GUI()
+
+const parameters = {
+	color: '#ffffff',
+    z_speed: 0.0025,
+    x_speed: 0.0005,
+    y_speed: 0.001
+}
+
+/*
+Cursor
+*/
+
 /*
     The javascrip code below gets the current position of the 
     mouse and the executes the defined function. The X-Value of the 
@@ -21,14 +38,20 @@ window.addEventListener('mousemove', (event) => {
     cursor.y = - (event.clientY / sizes.height - 0.5) 
 })
 
-// Canvas
+/* 
+Canvas
+*/
 const canvas = document.querySelector('canvas.webgl')
 
-// Scene
+/*
+Scene
+*/
 const scene = new THREE.Scene()
 
 
-// Objects
+/*
+Objects
+*/
 
 const geometry_torus = new THREE.TorusGeometry( 10, 0.4, 24, 96 )
 const material_torus = new THREE.MeshBasicMaterial( { color: 0xf5f5f5, transparent: true, opacity: 0.5 } )
@@ -44,7 +67,9 @@ const moon = new THREE.Mesh(
   })
 );
 
-// Cloning the objects
+/*
+Cloning the objects
+*/
 
 const moon_2 = moon.clone()
 const torus_2 = torus.clone()
@@ -60,7 +85,9 @@ torus_3.position.set(-15,5,25)
 moon_4.position.set(15,5,-25)
 torus_4.position.set(15,5,-25)
 
-// Adding the objects
+/*
+Adding the objects
+*/
 
 scene.add(torus)
 scene.add(moon)
@@ -72,12 +99,42 @@ scene.add(torus_4)
 scene.add(moon_4)
 
 /*
+Adding the objects to the debug menu (only one because I am to lazy to add all)
+*/
+
+gui.add(moon.position, 'y', -20, 20, 0.05).name('Moon Y') // The last three values are the minimum value, the maximum value and the step/ precision.
+gui.add(moon.position, 'x', -20, 20, 0.05).name('Moon X') // Alternativ we can use gui.add(moon.position, 'x').min(-20).max(20).step(0.05)
+gui.add(moon.position, 'z', -20, 20, 0.05).name('Moon Z')
+gui.add(moon, 'visible').name('Moon') // This allows toggeling the boolean that decides wheter to show or not show an Object
+gui.add(moon.material, 'wireframe').name('Moon wireframe') // This allows toggeling the wireframe of an object
+
+
+
+//gui.addColor(material_torus, '#ff00ff')
+
+gui.add(torus.position, 'y', -20, 20, 0.05).name('Torus Y')
+gui.add(torus.position, 'x', -20, 20, 0.05).name('Torus X')
+gui.add(torus.position, 'z', -20, 20, 0.05).name('Torus Z')
+gui.add(torus, 'visible').name('Torus')
+gui.add(torus.material, 'wireframe').name('Torus wireframe') 
+
+gui
+    .addColor(parameters, 'color')
+    .onChange(() => 
+    {
+        material_torus.color.set(parameters.color)
+    })
+
+/*
 const geometry_sphere = new THREE.SphereGeometry( 2.5, 25, 50 );
 const material_sphere = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
 const sphere = new THREE.Mesh( geometry_sphere, material_sphere );
 scene.add(sphere);
 */
-// Lights
+
+/*
+Lights
+*/
 
 const pointLight = new THREE.PointLight(0xD2AFFF);
 pointLight.position.set(3, 3, 3);
@@ -85,13 +142,18 @@ pointLight.position.set(3, 3, 3);
 const ambientLight = new THREE.AmbientLight(0xAB90F7);
 scene.add(pointLight, ambientLight);
 
-// Background
+/*
+Background
+*/
 
 const spaceTexture = new THREE.TextureLoader().load('Images/Space.jpg');
 scene.background = spaceTexture;
 
 
-// Sizes
+/*
+Sizes
+*/
+
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -147,61 +209,78 @@ window.addEventListener('dblclick', () =>
     }
 })
 
-// Camera
+/*
+Camera
+*/
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width/ sizes.height)
 camera.position.z = 15
 scene.add(camera)
 
-// Orbit Control
+/*
+Orbit Control
+*/
 
 // We pass the camera and canvas as input, where the canvas is the area of the website, on which interactions with
 // the camera can be triggered (e.g. camera can be moved or zoomed).
 const controls = new OrbitControls(camera, canvas) 
 controls.enableDamping = true
 
-// Renderer
+/*
+Renderer
+*/
+
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Clock
+/*
+Clock
+*/
+
 const clock = new THREE.Clock()
 
-// Animation/ frame update
+/*
+Animation/ frame update
+*/
 
 const tick = () =>
 {
-    // Clock
+    /*
+    Clock
+    */
+
     const elapsedTime = clock.getElapsedTime()
     
-    // Update the object
+    /*
+    Update the object
+    */
 
-    torus.rotation.z += 0.0025
-    torus.rotation.y += 0.0005
+    torus.rotation.z += parameters.z_speed
+    torus.rotation.y += parameters.x_speed
 
-    moon.rotation.z += 0.0025
-    moon.rotation.x += 0.001
+    moon.rotation.z += parameters.z_speed
+    moon.rotation.x += parameters.y_speed
 
-    torus_2.rotation.z -= 0.0025
-    torus_2.rotation.y -= 0.0005
+    torus_2.rotation.z -= parameters.z_speed
+    torus_2.rotation.y -= parameters.x_speed
 
-    moon_2.rotation.z -= 0.005
-    moon_2.rotation.x -= 0.001
+    moon_2.rotation.z -= parameters.z_speed
+    moon_2.rotation.x -= parameters.y_speed
 
-    torus_3.rotation.z += 0.0025
-    torus_3.rotation.y += 0.0005
+    torus_3.rotation.z += parameters.z_speed
+    torus_3.rotation.y += parameters.x_speed
 
-    moon_3.rotation.z += 0.005
-    moon_3.rotation.x += 0.001
+    moon_3.rotation.z += parameters.z_speed
+    moon_3.rotation.x += parameters.y_speed
 
-    torus_4.rotation.z -= 0.0025
-    torus_4.rotation.y -= 0.0005
+    torus_4.rotation.z -= parameters.z_speed
+    torus_4.rotation.y -= parameters.x_speed
 
-    moon_4.rotation.z -= 0.005
-    moon_4.rotation.x -= 0.001
+    moon_4.rotation.z -= parameters.z_speed
+    moon_4.rotation.x -= parameters.y_speed
 
 
   // Animation Loop
